f2ec615f999fd74d18ca7656b1077a822d8466e1
Esh <eshmeetkr@gmail.com>
2026-08-05 16:46:01 +0530

    version 1 inprog3

--- detector.py
+++ detector.py
@@ -1,5 +1,6 @@
 def analyze_email(email_text):
 
+#the dictionary containing scores and reasons 
     indicators = {
         "urgent": {
             "weight": 1,
@@ -16,20 +17,26 @@ def analyze_email(email_text):
             "reason": "Account suspension threat detected"
         }
     }
-
+#vars
     score = 0
     reasons = []
     max_score = 0
+
+#converting to lowercase to avoid duplicacy problems
     email_text = email_text.lower()
+
+#traversing dict and adding scores and reasons 
     for keyword, indicator in indicators.items():
         if keyword in email_text:
             score += indicators[keyword]["weight"]
             reasons.append(indicators[keyword]["reason"])
-
+            
+#calculating max score dynamically 
     for indicator in indicators.values():
         max_score += indicator["weight"]
-
         risk_percentage = (score / max_score) * 100
+
+    #for determining risk level- high med low, if block
     if risk_percentage < 30:
         risk_level = "Low"
     elif risk_percentage < 70:
